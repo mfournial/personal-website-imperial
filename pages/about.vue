6df56492c487ae17bf6bd2be5b2fc1226d3fc557
Mayeul Fournial <mfournial@outlook.com>
2019-04-08 13:51:46 +0100

    Write some legal mentions

---
+++ pages/about.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <section class="container">
+      <div class="container">
+        <div class="mt-3">
+          <strong class="text-center">I am not a GDPR expert but here are a few things I think I should write.</strong>
+        </div>
+        <br />
+        <h2>Legal Mentions</h2>
+        <p>Imperial records probably your IP along with the HTTP <em>Referer</em> & <em>User-Agent</em> and a bunch of other things you are sending when requesting this page for security reasons on their side. I don't set any cookie here, run advertisement or have any kind of <em>analytics</em> running so no annoying pop-up banner on this website! <a href="http://uncyclopedia.wikia.com/wiki/JavaScript">JavaScript</a> is required though to be able to download the compiled notes since to get you to download the latest version of the notes, the URL needs to be determined through GitHub's API (for the maths notes in particular).</p>
+        <p>You can find the code for this site in a <a href="https://github.com/mfournial/personal-website-imperial">GitHub repo</a> (<em>It may still be private though at this time, sorry :s</em>) if you like looking at HTML.</p>
+        <br />
+        <h2>About this website</h2>
+        <p>It was build using <a href="https://nuxtjs.org/">Nuxt.js</a> which is built on top of VueJS. It has been practical so far as a static website generator.</p>
+      </div>
+    </section>
+  </div>
+</template>
