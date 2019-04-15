05cd81c1bf50eb1ef7b7653d2211d54b027ae44f
Mayeul Fournial <mfournial@outlook.com>
2019-04-15 18:53:43 +0100

    Add link to m3p19 notes

--- pages/notes/year1.vue
+++ pages/notes/year1.vue
@@ -4,11 +4,12 @@
       <b-card no-body>
         <b-tabs card>
           <b-tab title="Mathematics" active>
-            <div>
+            <div class="container">
               <b-button variant="link" v-on:click="fetch_notes" id="notes_test">Test notes</b-button>
+              <b-button variant="link" v-on:click="m3p19" id="m3p19">Measure</b-button>
             </div>
           </b-tab>
-          <b-tab title="Computing" active>
+          <b-tab title="Computing">
             Computing module
           </b-tab>
         </b-tabs>
@@ -18,33 +19,46 @@
 </template>
     
 <script>
-    export default {
-        data() {
-            return {
-            }
-        },
-        head() {
-            return {
-                script: [
-                    { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
-                ],
-            }
-        },
-        methods: {
-            // On pressing the "get notes button"
-            fetch_notes: function (event) {
-                console.log(event.srcElement.id);
-                console.log(this.$refs.notes_test);
-                $.getJSON('https://api.github.com/repos/icl-notes/notes-test/releases/latest', function(data) {
-                  var tag = data.tag_name;
-                  // var tag = "untagged-12a8e737d2822d8c073f";
-                  var down = "https://github.com/icl-notes/notes-test/releases/download/" + tag + "/test.pdf";
-                  window.open(down, "self");
+export default {
+    data() {
+        return {
+        }
+    },
+    head() {
+        return {
+            script: [
+                { src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js' },
+            ],
+        }
+    },
+    methods: {
+        // On pressing the "get notes button"
+        fetch_notes: function (event) {
+            console.log(event.srcElement.id);
+            console.log(this.$refs.notes_test);
+            $.getJSON('https://api.github.com/repos/icl-notes/notes-test/releases/latest', function(data) {
+                var tag = data.tag_name;
+                // var tag = "untagged-12a8e737d2822d8c073f";
+                var down = "https://github.com/icl-notes/notes-test/releases/download/" + tag + "/test.pdf";
+                window.open(down, "self");
                 // data[tag_name] === "untagged-12a8e......"
                 // URL to match:
                 // https://github.com/icl-notes/notes-test/releases/download/untagged-12a8e737d2822d8c073f/test.pdf    
-                });
+            });
+        },
+        m3p19: function (event) {
+            console.log(event.srcElement.id);
+            console.log(this.$refs.notes_test);
+            $.getJSON('https://api.github.com/repos/icl-notes/m3p19/releases/latest', function(data) {
+                var tag = data.tag_name;
+                // var tag = "untagged-12a8e737d2822d8c073f";
+                var down = "https://github.com/icl-notes/m3p19/releases/download/" + tag + "/notes.pdf";
+                window.open(down, "self");
+                // data[tag_name] === "untagged-12a8e......"
+                // URL to match:
+                // https://github.com/icl-notes/notes-test/releases/download/untagged-12a8e737d2822d8c073f/test.pdf    
+            });
         }
-  }
+    }
 }
 </script>
