c55169f54152f85a752d41a03a5a716bfe5463ad
Mayeul Fournial <mfournial@outlook.com>
2019-04-15 15:39:53 +0100

    Finish the layout of the navbar

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,17 +13,19 @@
             </b-nav-item-dropdown>
             <b-nav-item to="/projects">Projects and Interests</b-nav-item>
             <b-nav-item to="/CV/cv-mayeul-FOURNIAL.pdf">CV</b-nav-item>
+            <b-nav-item to="/about">
+              <em>About</em>
+            </b-nav-item>
           </b-navbar-nav>
 
           <!-- Right aligned nav items -->
           <b-navbar-nav class="ml-auto">
+            <b-nav-item href="https://github.com/mfournial"><img src="github-light.png" alt="GitHub" /></b-nav-item>
+            <b-nav-item href="https://linkedin.com/in/mfournial"><img src="linkedin-light.png" alt="LinkedIn" /></b-nav-item>
             <b-nav-form @submit="duckThis">
-              <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Troll search" v-model="search.input" required />
+              <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="LM(DD)GTFY" v-model="search.input" required />
               <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
             </b-nav-form>
-            <b-nav-item to="/about">
-              <em>About</em>
-            </b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-navbar>
