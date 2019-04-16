21b89c10e21c0c2a4fc2ef5cb7f3d8858606e282
Mayeul Fournial <mfournial@outlook.com>
2019-04-16 15:40:48 +0100

    Write up index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,27 @@
 <template>
   <section class="container">
-    <div>
-      <b-button v-b-popover.hover="'popover content!'" title="Popover Title">Hover Me</b-button>
-    </div>
-    <div>
-      <h2>
-        Mayeul Fournial is a motivated, hardworking and reliable penultimate year student at Imperial College London on track to achieve a first class degree. He is also the academic representative of in the third year of his course Joint Maths and Computing.
-      </h2>
-      
-      <div class="text-center">
-        <b-spinner label="Spinning" />
-        <b-spinner type="grow" label="Spinning" />
-        <b-spinner variant="primary" label="Spinning" />
-        <b-spinner variant="primary" type="grow" label="Spinning" />
-        <b-spinner variant="success" label="Spinning" />
-        <b-spinner variant="success" type="grow" label="Spinning" />
+    <div class="container">
+      <div class="mt-3 text-center text-dark container">
+        <h2>
+        <b-spinner variant="danger" type="grow" label="Spinning" />
+          Good {{ timeGreeting(new Date().getHours()) }}
+        <b-spinner variant="danger" type="grow" label="Spinning" />
+        </h2>
       </div>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/mfournial"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
+      <h3 class="text-info">Imperial notes</h3>
+      <div class="text-justify container">
+        <p>
+          You'll find on here the links to the <nuxt-link to="/notes/year1">year 1</nuxt-link>, <nuxt-link to="/notes/year2">year 2</nuxt-link>, and <nuxt-link to="/notes/years3-4">year 3 & 4</nuxt-link> notes. You can choose to simply download them or contribute to help maintaining them. A special thanks to all the students who've helped putting them up. (TODO: list peeps)
+        </p>
+      </div>
+      <h3 class="text-info">About me</h3>
+      <div class="text-justify container">
+        <p>
+        I am a penultimate year student at Imperial College London studying Joint Maths and Computing (<a href="https://www.imperial.ac.uk/computing/current-students/jmc-info/">JMC MEng</a>). I am the current academic year representative of my cohort and was elected to be the departmental academic representative of JMC for the year 2019/2020. I'm also a tennis fan <span v-on:click="changeAnimationStatus">({{ flag1 }} > {{ flag2 }} > {{ flag3 }})</span> and enjoy playing the piano 🎹.
+        </p>
+        <p>
+          I am on track to achieve a first class degree and I received two prizes for my work at Imperial. I was awarded the Olav Beckmann prize at the end of second year for outstanding second year undergraduate laboratory project work as well as the David Howarth prize for the Voodoo database. Please see my projects on <a href="https://linkedin.com/in/mfournial">LinkedIn</a> or my <nuxt-link to="/CV/cv-mayeul-FOURNIAL.pdf">CV</nuxt-link>, and Imperial's <a href="http://www.imperial.ac.uk/computing/prospective-students/distinguished-projects/ug-prizes/"> project page</a> and <a href="https://www.imperial.ac.uk/computing/current-students/project-info/ug-prizes/archive/"> archive</a> for more details.
+        </p>
       </div>
     </div>
   </section>
@@ -38,6 +33,10 @@ export default {
         return {
             description: "Mayeul Fournial is a motivated, hardworking and reliable penultimate year student at Imperial College London on track to achieve a first class degree. He is also the academic representative of in the third year of his course Joint Maths and Computing.",
             title: "Index",
+            flag1: "🇨🇭",
+            flag2: "🇷🇸",
+            flag3: "🇪🇸",
+            animate: true,
         }
     },
     head () {
@@ -47,7 +46,34 @@ export default {
                 { hid: "description", name: "description", content: this.description },
             ],
         }
-    }
+    },
+    methods: {
+        timeGreeting: function(currentHour) {
+            if (4 <= currentHour && currentHour < 12) {
+                return "morning";
+            } else if (12 <= currentHour && currentHour < 19) {
+                return "afternoon";
+            } else if (19 <= currentHour && currentHour < 23) {
+                return "evening";
+            } else {
+                return "night?? It's late to be up here";
+            }
+        },
+        animateFlags: function() {
+            if (this.animate) {
+                var tmp = this.flag1;
+                this.flag1 = this.flag2;
+                this.flag2 = this.flag3;
+                this.flag3 = tmp;
+            }
+        },
+        changeAnimationStatus: function() {
+            this.animate = !this.animate;
+        }
+    },
+    beforeMount() {
+        setInterval(this.animateFlags, 600);
+    },
 }
 </script>
 
